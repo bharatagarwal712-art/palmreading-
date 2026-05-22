27c967f1f811b83c69b239970717d15630959e57
bharatagarwal712-art <bharatagarwal712@gmail.com>
2026-05-22 16:09:11 +0530

    Remove legacy app shell and simplify root upload flow

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { AppShell } from "@/components/layout/app-shell";
 import { PremiumLandingPage } from "@/components/sections/premium-landing-page";
 
 export default function HomePage() {
-  return (
-    <AppShell hideMobileNav>
-      <PremiumLandingPage />
-    </AppShell>
-  );
+  return <PremiumLandingPage />;
 }
