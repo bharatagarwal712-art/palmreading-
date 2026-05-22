bbb4a07c16c537dbb7f3842b35d55f07d885a836
bharatagarwal712-art <bharatagarwal712@gmail.com>
2026-05-22 13:11:03 +0530

    Add MediaPipe hand landmark detector

---
+++ lib/palm/detect-hand.ts
@@ -0,0 +1,18 @@
+import { Hands } from "@mediapipe/hands";
+
+export async function createHandDetector() {
+  const hands = new Hands({
+    locateFile: (file) => {
+      return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+    },
+  });
+
+  hands.setOptions({
+    maxNumHands: 1,
+    modelComplexity: 1,
+    minDetectionConfidence: 0.7,
+    minTrackingConfidence: 0.7,
+  });
+
+  return hands;
+}
