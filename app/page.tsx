b6e012d07fd455da59125507cf2b78e1caf31910
bharatagarwal712-art <bharatagarwal712@gmail.com>
2026-05-27 18:44:48 +0530

    Add session continuity widgets to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,13 @@
 import { PremiumLandingPage } from "@/components/sections/premium-landing-page";
+import { SessionWidget } from "@/components/session-widget";
+import { ContinueReadingWidget } from "@/components/continue-reading-widget";
 
 export default function HomePage() {
-  return <PremiumLandingPage />;
+  return (
+    <>
+      <SessionWidget />
+      <ContinueReadingWidget />
+      <PremiumLandingPage />
+    </>
+  );
 }
