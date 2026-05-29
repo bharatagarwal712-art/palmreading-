7ed61c1d0e92ec9ba662baee91540ab56a0b3825
bharatagarwal712-art <bharatagarwal712@gmail.com>
2026-05-29 15:16:55 +0530

    Update session-widget.tsx

--- components/session-widget.tsx
+++ components/session-widget.tsx
@@ -32,7 +32,7 @@ const handleSignIn = async () => {
 await supabase.auth.signInWithOAuth({
 provider: "google",
 options: {
-redirectTo: ${window.location.origin}/,
+redirectTo: `${window.location.origin}/`,
 },
 });
 };
